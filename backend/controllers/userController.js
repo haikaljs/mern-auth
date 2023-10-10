3faafd5cc0edd3969471cad8334cc7456d642648
haikaljs <haikaljavascript@gmail.com>
2023-10-10 18:33:38 +0800

    custom errorMiddleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,8 @@ import asyncHandler from "express-async-handler";
 // @access public
 
 const authUser = asyncHandler((req, res) => {
+//   res.status(401);
+//   throw new Error("Something went wrong!");
   res.status(200).json({ message: "Auth user" });
 });
 
